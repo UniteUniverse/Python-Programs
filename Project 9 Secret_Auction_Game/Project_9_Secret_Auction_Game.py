d9136f68c39b19d9ff4641820e3bc0a29c691d68
Pratyush Kumar Jha <122214040+UniteUniverse@users.noreply.github.com>
2024-09-07 19:21:42 +0530

    Update Project_9_Secret_Auction_Game.py
    Removed print statement

--- Project 9 Secret_Auction_Game/Project_9_Secret_Auction_Game.py
+++ Project 9 Secret_Auction_Game/Project_9_Secret_Auction_Game.py
@@ -19,13 +19,13 @@ name=input("Enter your name: ")
 bid_amount=input("Enter your amount in $: ")
 auction={name:bid_amount,}
 bidding=input('Do you want to add more bidding: Type "Yes" or "No": ')
-clear()
+
 while bidding=="Yes":
     name=input("Enter your name: ")
     bid_amount=input("Enter your amount in $: ")
     bidding=input('Do you want to add more bidding: Type "Yes" or "No"')
     clear()
     auction[name]=bid_amount
-print(auction)
+
 Keymax = max(auction, key= lambda x: auction[x])
 print(f"The highest bid is of {Keymax}")
