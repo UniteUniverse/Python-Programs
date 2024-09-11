888d6f6195bb4c466c6f35bfbd5e8cb5adad1b57
Pratyush Kumar Jha <122214040+UniteUniverse@users.noreply.github.com>
2024-09-11 17:53:24 +0530

    Update main.py

--- Project 27 ISS_Overhead/main.py
+++ Project 27 ISS_Overhead/main.py
@@ -4,8 +4,8 @@ import smtplib
 import time
 MY_LAT = 28.613939
 MY_LONG = 77.209023
-my_email="t9940472@gmail.com"
-password="vbqj xpse uvtk ncmm"
+my_email="" #Enter your email
+password="" #Get Your Password
 def iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
@@ -40,11 +40,11 @@ while True:
             connection.starttls()
             connection.login(user=my_email, password=password)
             connection.sendmail(from_addr=my_email, to_addrs="pratyush.snj@gmail.com",msg=f"Subject:Look Up\n\nThe ISS is above you in the sky.")
+    else:
+        print('Try Later')
+        break
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+
 
 
 
